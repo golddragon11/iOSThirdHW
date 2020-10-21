d49c5eb3599fa9f9efa40b898360fd88eafc784c
Steve Shen <golddragon11@gmail.com>
2020-10-21 10:58:48 +0800

    Initial Commit

---
+++ iOSThirdHW/iOSThirdHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSThirdHWApp.swift
+//  iOSThirdHW
+//
+//  Created by Steve Shen on 2020/10/21.
+//
+
+import SwiftUI
+
+@main
+struct iOSThirdHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
